8c46b101dc2ed36c707cde8be1845e7f8f09aa3f
Sagar Chamling <sagarchamling@lftechnology.com>
2018-06-04 23:21:16 +0545

    Update requirement.txt, add alembic

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from config import config
 def create_app(flask_config='development'):
     app = Flask(__name__)
     app.config.from_object(config[flask_config])
+
     # Bluprints registration
     from app.api import api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
