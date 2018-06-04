bcb03b497b9749b5e5b63e8abf1796d6913efdee
Robus Gauli <robusgauli@gmail.com>
2018-06-04 14:30:46 +0545

    Added lib directory

---
+++ lib/config.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class ConfigMetaType(type):
+    _config_registry = defaultdict(dict)
+
+    def __new__(cls, clsname, bases, clsdict):
+        _class_instance = super().__new__(cls, clsname, bases, clsdict)
+        if '__name__' in clsdict:
+            cls._config_registry[clsdict['__name__']] = _class_instance
+        return _class_instance
+    
+    @classmethod
+    def create_config(cls):
+        return cls._config_registry
+
+class ConfigBase(metaclass=ConfigMetaType):
+    pass
+
+__all__ = ['ConfigBase']
