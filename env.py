b1699fe7078f3bd5d64b514f7f58486af2ce3e28
Sagar Chamling <sagarchamling@lftechnology.com>
2018-06-05 21:21:04 +0545

    Fix .env file path

--- env.py
+++ env.py
@@ -16,4 +16,4 @@ def initialize_env():
     env_path = os.path.join(env_dir_path, '.env')
     if not os.path.exists(env_path):
         raise FileNotFoundError('Environment file does not exists.')
-    load_dotenv(os.path.join(env_path, '.env'))
+    load_dotenv(env_path)
